b9d63208a642f2ef973e7d0be8b1758c9bec31a9
David Maple <david@cyrup.ai>
2025-11-13 15:59:10 -0800

    v0.2.1 :: infinite keepalive

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub async fn start_server(
     };
 
     let shutdown_timeout = Duration::from_secs(30);
-    let session_keep_alive = Duration::from_secs(300);
+    let session_keep_alive = Duration::ZERO;
 
     create_http_server("process", addr, tls_config, shutdown_timeout, session_keep_alive, |_config, _tracker| {
         Box::pin(async move {
